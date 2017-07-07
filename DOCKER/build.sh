4b742c4b2bef872f79aa52ddd4571f5be32452b6
Marko <marko.eremija@amres.ac.rs>
2017-07-07 18:16:12 +0200

    Build a Docker image
    Builds a Docker image and assings a tag. Dockerfile should be inside the same directory.

---
+++ DOCKER/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ $1 == "tag" ]]; then
+    TAG=`date +:%Y%m%d-%H%M%S`
+elif [[ -n $1 ]]; then
+    TAG=":$1"
+else
+    TAG=":latest"
+fi
+
+docker image build -t stretch-shib-idp-test01$TAG .
