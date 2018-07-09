3f98ee2405309fea42543d898d1bb78479ae5ebf
Marko <marko.eremija@amres.ac.rs>
2018-07-09 11:39:40 +0200

    Clean the Dockerfile and other config
    Clean the Dockerfile and other configuration.

--- DOCKER/phpLDAPadmin/entrypoint.sh
+++ DOCKER/phpLDAPadmin/entrypoint.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
 target_dir=/opt
 cwd=`pwd`
 
 # Password for LDAP admin
-ldap_root_pw="test123";
+_pla_root_pw="testing123"
+_org="AMRES"
+_hostname="docker-test.amres.ac.rs"
+_basedn="dc=amres,dc=ac,dc=rs"
+_rootdn="cn=admin,dc=amres,dc=ac,dc=rs"
+_rootpw="password"
 
 if [ "$cwd" != "$target_dir" ]; then
    cd /opt
@@ -14,10 +20,19 @@ fi
 tar xf phpldapadmin.tar.gz
 
 # Copy config.php file
-cp config.php ./phpldapadmin/config/config.php
+cp config.php ./phpldapadmin/config/config.php && \
+chmod 644 ./phpldapadmin/config/config.php && \
+chown root:root ./phpldapadmin/config/config.php
+
+# Replace multiple values inside the same config file using "sed"
+
+find ./ -name "*.php" | xargs sed -i 's/Local LDAP Server/'"${_org}"'/g'
+find ./ -name "*.php" | xargs sed -i 's/127\.0\.0\.1/'"${_hostname}"'/g'
+find ./ -name "*.php" | xargs sed -i 's/dc\=example,dc\=org/'"${_basedn}"'/g'
+find ./ -name "*.php" | xargs sed -i 's/secret/'"${_rootpw}"'/g'
 
 # Create a user named "admin" and use LDAP root password parameter
-htpasswd -b -c /etc/apache2/htpasswd admin "$ldap_root_pw"
+htpasswd -b -c /etc/apache2/htpasswd admin "${_pla_root_pw}"
 
 # Copy custom phpLDAPadmin configuration file to Apache conf-enabled dir
 cp phpldapadmin.conf /etc/apache2/conf-enabled/phpldapadmin.conf
@@ -29,7 +44,7 @@ chmod 644 /etc/apache2/conf-enabled/idm.conf && \
 chown root:root /etc/apache2/conf-enabled/idm.conf
 
 # Copy institution logo and change permissions
-cp logo.png ./phpldapadmin/htdocs/images/default/logo-small.png
+cp iAMRES-josmanji.png ./phpldapadmin/htdocs/images/default/logo-small.png
 
 chmod 644 ./phpldapadmin/htdocs/images/default/logo-small.png && \
 chown root:root ./phpldapadmin/htdocs/images/default/logo-small.png
@@ -63,25 +78,25 @@ chown root:root ./phpldapadmin/htdocs/action_lock.php
 # Copy easy identity templates
 
 cp create_idpAccount_easy.xml ./phpldapadmin/templates/creation/custom_idpAccount.xml && \
-chmod 640 ./opt/phpldapadmin/templates/creation/custom_idpAccount.xml && \
-chown root:www-data ./opt/phpldapadmin/templates/creation/custom_idpAccount.xml
+chmod 640 /opt/phpldapadmin/templates/creation/custom_idpAccount.xml && \
+chown root:www-data ./phpldapadmin/templates/creation/custom_idpAccount.xml
 
-cp modify_idpAccount_easy.xml ./phpldapadmin/templates/modification/custom_idpAccount.xml && \
-chmod 640 ./opt/phpldapadmin/templates/modification/custom_idpAccount.xml && \
-chown root:www-data ./phpldapadmin/templates/modification/custom_idpAccount.xml
+cp modify_idpAccount_easy.xml ./phpldapadmin/templates/modification/modify_idpAccount.xml && \
+chmod 640 /opt/phpldapadmin/templates/modification/modify_idpAccount.xml && \
+chown root:www-data ./phpldapadmin/templates/modification/modify_idpAccount.xml
 
 # Copy full identity templates if easy identity templates are not present
 
 if  ! { [ -f /opt/phpldapadmin/templates/creation/custom_idpAccount.xml ] && \
-      [ -f /opt/phpldapadmin/templates/modification/custom_idpAccount.xml ]; }; then
+      [ -f /opt/phpldapadmin/templates/modification/modify_idpAccount.xml ]; }; then
 
   cp create_idpAccount.xml ./phpldapadmin/templates/creation/custom_idpAccount.xml && \
   chmod 640 ./phpldapadmin/templates/creation/custom_idpAccount.xml && \
   chown root:www-data ./phpldapadmin/templates/creation/custom_idpAccount.xml
 
-  cp modify_idpAccount.xml ./phpldapadmin/templates/modification/custom_idpAccount.xml && \
-  chmod 640 ./phpldapadmin/templates/modification/custom_idpAccount.xml && \
-  chown root:www-data ./phpldapadmin/templates/modification/custom_idpAccount.xml
+  cp modify_idpAccount.xml ./phpldapadmin/templates/modification/modify_idpAccount.xml && \
+  chmod 640 ./phpldapadmin/templates/modification/modify_idpAccount.xml && \
+  chown root:www-data ./phpldapadmin/templates/modification/modify_idpAccount.xml
 
 else
   printf "Easy IdM templates have been copied.\n"
@@ -96,4 +111,12 @@ cp index.php ./phpldapadmin/htdocs/idm-tools/index.php
 chown root:root ./phpldapadmin/htdocs/idm-tools/index.php && \
 chmod 644 ./phpldapadmin/htdocs/idm-tools/index.php
 
+# Copy custom CA certificate and add it to the chain of other CA certificates
+
+cp /opt/phpldapadmin/config/cacert.pem /usr/local/share/ca-certificates/cacert.crt && \
+cd /usr/local/share/ca-certificates/ && \
+/usr/sbin/update-ca-certificates
+
 # Run apache2
+
+exec "$@"
